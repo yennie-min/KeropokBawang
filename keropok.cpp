77fb664a134dcedbba1b9e352e2487aa91c23c6a
yenniemin <yenniemin@gmail.com>
2022-09-04 20:31:08 +0800

    First commit

---
+++ keropok.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    float a = 5;
+
+     do {
+        cout << "Berapa CGPA awak? Jawapan dalam satu perpuluhan.";
+
+        cin >> a;
+
+        if ( a < 2 ) {
+            cout << "Keje je la";
+        }
+        else if ( a >= 2 && a < 3) {
+            cout << "Oh...alhamdulillah";
+        }
+        else if ( a >= 3 && a <= 4) {
+            cout << "Amboii kau. Kemain. Kau tuition mana tu? Berapa tu?";
+        }
+        else {
+            cout << "Kau pandai baca ke tak.Baca lah betul-betul.";
+        }
+
+    }  while ( a <= 4 );
+
+    
+
+
+    return 0;
+}
